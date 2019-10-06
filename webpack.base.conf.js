1359bd6a6547e7f6ec1edd2a139b856a405891e2
傅诚 <327619852@qq.com>
2019-10-06 21:43:36 +0800

    add babel

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -17,8 +17,10 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       }
     ]
   },
