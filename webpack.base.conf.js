8c8f8565ac88911dba4bc19668deb32259ac27ed
傅诚 <327619852@qq.com>
2019-10-07 12:40:52 +0800

    add ts

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -21,8 +21,16 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       }
     ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
   },
   plugins: [
     new HtmlWebpackPlugin({
